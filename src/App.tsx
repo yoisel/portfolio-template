32483ee55923d134f2c62a56a34184a6b240e745
yoisel <yoisel@users.noreply.github.com>
2023-04-24 18:08:06 -0400

    Adding prettier, some style improvements, some additions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
-import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
-import { useEffectOnce } from "usehooks-ts";
+import { createTheme, ThemeProvider, CssBaseline } from '@mui/material';
+import { useEffectOnce } from 'usehooks-ts';
 
-import { themeOptions } from "./themes/grayscale";
-import { AppHeader } from "./components/AppHeader";
-import { AppContents } from "./components/AppContents";
-import { appConfig } from "./model/AppData";
+import { themeOptions } from './themes/grayscale';
+import { AppHeader } from './components/AppHeader';
+import { AppContents } from './components/AppContents';
+import { appConfig } from './model/AppData';
 
 const theme = createTheme(themeOptions);
 
 const App = () => {
-    useEffectOnce(() => {
-        const docTitle = document.getElementById('app-title') || { textContent: '' };
-        docTitle.textContent = appConfig.title;
-    });
+  useEffectOnce(() => {
+    const docTitle = document.getElementById('app-title') || {
+      textContent: ''
+    };
+    docTitle.textContent = appConfig.title;
+  });
 
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <AppHeader {...appConfig} />
-            <AppContents {...appConfig} />
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppHeader {...appConfig} />
+      <AppContents {...appConfig} />
+    </ThemeProvider>
+  );
 };
 
 export default App;

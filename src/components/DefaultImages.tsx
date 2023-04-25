fd8a8d9de6db6f685bea66b991819133822463e0
yoisel <yoisel@users.noreply.github.com>
2023-04-25 00:52:26 -0400

    Personalizing for myself

--- src/components/DefaultImages.tsx
+++ src/components/DefaultImages.tsx
@@ -6,19 +6,45 @@ import networks from '../assets/ai-generated/networks.jpg';
 import signals from '../assets/ai-generated/sginals.jpg';
 import signalsDark from '../assets/ai-generated/signals-dark.jpg';
 import universe from '../assets/ai-generated/universe.jpg';
-import education from '../assets/ai-generated/education.jpg';
+import beachDark from '../assets/ai-generated/beach-dark.jpg';
 
-import avatar from '../assets/avatar.png';
+import education from '../assets/ai-generated/education.jpg';
+import concordia from '../assets/logos/concordia.png';
+import uhavana from '../assets/logos/universidad_de_la_habana.png';
+
+import amyuni from '../assets/logos/amyuni.png';
+import gmixon from '../assets/logos/gmixon.png';
+import HPE from '../assets/logos/Hewlett_Packard_Enterprise.png';
+import ICRT from '../assets/logos/icrt.png';
+import avatar from '../assets/avatar-yms.jpg';
+import coffee from '../assets/cubita.jpg';
+
+import aos from '../assets/portfolio/aruba-os.png';
+import newdj from '../assets/portfolio/newdj.png';
+import radiosys from '../assets/portfolio/radiosys.bmp';
+import amyuniPdf from '../assets/portfolio/amyuni-pdf-converter.png';
 
 export class DefaultImages {
   static MathDark2 = mathDark2;
   static MathDark1 = mathDark1;
   static Music = music;
   static Education = education;
+  static Concordia = concordia;
+  static UH = uhavana;
   static Math = math;
   static Networks = networks;
   static Signals = signals;
   static SignalsDark = signalsDark;
   static Universe = universe;
+  static Amyuni = amyuni;
+  static Gmixon = gmixon;
+  static HPE = HPE;
+  static ICRT = ICRT;
   static Avatar = avatar;
+  static Coffee = coffee;
+  static BeachDark = beachDark;
+  static RadioSys = radiosys;
+  static AOS = aos;
+  static NewDJ = newdj;
+  static AmyuniPdfConverter = amyuniPdf;
 }
--- src/model/AppData.tsx
+++ src/model/AppData.tsx
@@ -3,46 +3,29 @@ import { AppConfig } from './AppData.interface';
 
 // json-like object for app configuration
 export const appConfig: AppConfig = {
-  title: 'Fulano Mengano',
+  title: "Yoisel's Portfolio",
   avatarImage: DefaultImages.Avatar,
   social: {
-    github: 'https://github.com/random',
-    linkedin: 'https://www.linkedin.com/in/random/',
-    stackoverflow: 'https://stackoverflow.com/users/000000/random'
+    github: 'https://github.com/yoisel',
+    linkedin: 'https://www.linkedin.com/in/yoisel/',
+    stackoverflow: 'https://stackoverflow.com/users/501196/yms'
   },
   sections: [
     {
-      id: 'experience',
-      title: 'Work Experience',
-      backgroundImage: DefaultImages.MathDark1,
+      id: 'about',
+      title: 'About Me',
+      backgroundImage: DefaultImages.BeachDark,
       portfolioPageItems: [
         {
-          subTitle1: '2015-Today',
-          title: 'CEO',
-          subTitle2: 'Skynet Corp',
-          link: 'https://www.skynet.com',
-          linkText: 'www.skynet.com',
           contents: [
-            "Look again at that dot. That's here. That's home. That's us. On it everyone you love, everyone you know, everyone you ever heard of, every human being who ever was, lived out their lives.",
-            'The aggregate of our joy and suffering, thousands of confident religions, ideologies, and economic doctrines, every hunter and forager, every hero and coward, every creator and destroyer of civilization, every king and peasant, every young couple in love, every mother and father, hopeful child, inventor and explorer, every teacher of morals, every corrupt politician, every "superstar," every "supreme leader," every saint and sinner in the history of our species lived there--on a mote of dust suspended in a sunbeam.'
+            `I am a Full Stack software developer with more than 15 years of experience, including a few leading teams of developers. 
+                     I am particularly conscious about security and code quality in the products I work on. 
+                     I enjoy solving problems, facing challenges and learning new technologies.`,
+            'I like biking, light jogging, science-fiction - fantasy books and films.',
+            'I learned my first programming language (Logo) at age 11, then moved into MSX Basic and Pascal by age 16. I found my passion and I have never looked back since then.',
+            'I was born in Cuba, an island in the Caribbean Sea, and I currently live in Montreal, Canada.'
           ],
-          image: DefaultImages.Networks
-        },
-        {
-          subTitle1: '2005-2010',
-          title: 'Lecturer, Large Language Models course',
-          subTitle2: 'Stelvia University',
-          contents:
-            'We choose to go to the moon. We choose to go to the moon in this decade and do the other things, not because they are easy, but because they are hard, because that goal will serve to organize and measure the best of our energies and skills, because that challenge is one that we are willing to accept, one we are unwilling to postpone, and one which we intend to win, and the others, too.',
-          image: DefaultImages.Signals
-        },
-        {
-          subTitle1: '2000-2005',
-          title: 'Teaching Assistant',
-          subTitle2: 'Stelvia University',
-          contents:
-            "In the last 24 hours we have seen facilities now being created for the greatest and most complex exploration in man's history. We have felt the ground shake and the air shattered by the testing of a Saturn C-1 booster rocket, many times as powerful as the Atlas which launched John Glenn, generating power equivalent to 10,000 automobiles with their accelerators on the floor. We have seen the site where the F-1 rocket engines, each one as powerful as all eight engines of the Saturn combined, will be clustered together to make the advanced Saturn missile, assembled in a new building to be built at Cape Canaveral as tall as a 48 story structure, as wide as a city block, and as long as two lengths of this field.",
-          image: DefaultImages.Education
+          image: DefaultImages.Coffee
         }
       ]
     },
@@ -52,42 +35,143 @@ export const appConfig: AppConfig = {
       backgroundImage: DefaultImages.Universe,
       portfolioPageItems: [
         {
-          subTitle1: '2005-2006',
-          title: 'Stelvia University',
+          subTitle1: '2010 - 2011',
+          title: 'Concordia University, Montreal, Canada',
           subTitle2: 'Master in Applied Computer Science',
-          contents:
-            'We choose to go to the moon. We choose to go to the moon in this decade and do the other things, not because they are easy, but because they are hard, because that goal will serve to organize and measure the best of our energies and skills, because that challenge is one that we are willing to accept, one we are unwilling to postpone, and one which we intend to win, and the others, too.',
-          image: DefaultImages.Education
+          link: 'https://www.concordia.ca/',
+          linkText: 'www.concordia.ca',
+          contents: '',
+          image: DefaultImages.Concordia
         },
         {
           subTitle1: '2001-2004',
-          title: 'Cosmic University',
+          title: 'Havana University (Universidad de la Habana)',
           subTitle2: 'Bachalor in Computer Science',
-          contents:
-            'There is no strife, no prejudice, no national conflict in outer space as yet. Its hazards are hostile to us all. Its conquest deserves the best of all mankind, and its opportunity for peaceful cooperation many never come again. But why, some say, the moon? Why choose this as our goal? And they may well ask why climb the highest mountain? Why, 35 years ago, fly the Atlantic? Why does Rice play Texas?',
-          image: DefaultImages.Education
+          link: 'https://www.uh.cu',
+          contents: '',
+          image: DefaultImages.UH
+        }
+      ]
+    },
+    {
+      id: 'experience',
+      title: 'Work Experience',
+      backgroundImage: DefaultImages.MathDark1,
+      portfolioPageItems: [
+        {
+          subTitle1: 'April 2016 - Today',
+          title: 'Senior Software Developer / Tech Lead',
+          subTitle2: 'Hewlett Packard Enterprise - Montreal, Canada',
+          link: 'https://www.hpe.com',
+          linkText: 'www.hpe.com',
+          // \u2022 unicode character for bullet point
+          contents: [
+            '\u2022 Technical leading on the development of a React-based components library for Aruba Central, a cloud-based Network Management System, with a distributed team across multiple geographies.',
+            '\u2022 Development of software modules for Aruba Central and Aruba Instant On.',
+            '\u2022 Development of software modules for Aruba OS, a Linux-based embedded operating system for managed wireless networks.',
+            '',
+            'Technologies used:',
+            '    React, MUI, styled-components, Storybook, Cypress, Jenkins, TypeScript, HTML, CSS, SCSS,',
+            '    Docker, Angular, AngularJS, Javascript, ES6, NodeJS, Java, Linux-based Embedded Systems,',
+            '    ANSI C, Network protocols, Wifi (802.11) specifications.'
+          ],
+          image: DefaultImages.HPE
+        },
+        {
+          subTitle1: '2009 - 2016',
+          title: 'Senior Software Developer / Team Lead',
+          subTitle2: 'Amyuni Technologies',
+          link: 'https://www.amyuni.com',
+          linkText: 'www.amyuni.com',
+          contents: [
+            `Amyuni Technologies is a software company specialized in the development of applications and libraries for end users and developers that facilitates processing and producing digital documents such as PDF, PostScript and Microsoft XPS.`,
+            '\u2022 Team leader since January 2013, controlling the development process and keeping track of the status of each project, planning and prioritizing the tasks for the development team, establishing and updating the protocol for quality assurance and participating in the hiring process for new developers.',
+            '\u2022 Development of software modules for Amyuni PDF libraries using several technologies and programming languages (.net, C#, C, C++, ActiveX, Windows Driver Kit, Task Parallel Libraries, Async-programming).',
+            '\u2022 Development of a JavaScript interpreter and its corresponding Document Object Model for PDF dynamic forms (C++, yacc, bison, ECMA-Script specifications).',
+            '\u2022 Development of a Windows Embedded CE platform with several customized software modules.',
+            '\u2022 Development of several software modules for iOS and Mac OS X.'
+          ],
+          image: DefaultImages.Amyuni
+        },
+        {
+          subTitle1: '2001-2009',
+          title: 'Senior Software Developer / Team Lead',
+          subTitle2: 'GMixon (remote from Cuba)',
+          link: 'https://www.gixon.com',
+          linkText: 'www.gmixon.coom',
+          contents: [
+            'Development of desktop-based client/server systems, web applications, digital audio and digital video broadcasting systems, and mobile applications.',
+            'Team leader during the development of several projects.'
+          ],
+          image: DefaultImages.Gmixon
+        },
+        {
+          subTitle1: '2005-2009',
+          title: 'Software Developer',
+          subTitle2: 'Cuban Institute of Radio and Television (ICRT)',
+          link: 'https://www.icrt.gob.cu',
+          linkText: 'www.icrt.gob.cu',
+          contents: [
+            'Development of applications that provided content management, multi-channel audio playback support, and sound effects for radio stations.',
+            'These applications were built using technologies such as C++, Delphi Win32, DirectX, .Net Framework/C#, and Postgresql. Additionally, I provided technical consulting on digital audio and video topics.'
+          ],
+          image: DefaultImages.ICRT
+        },
+        {
+          subTitle1: '2003-2005',
+          title: 'Lecturer / Teaching Assistant',
+          subTitle2: 'Havana University, Faculty of Mathematics and Computer Science',
+          link: 'https://www.uh.cu',
+          linkText: 'www.uh.cu',
+          contents: [
+            '\u2022 Lecturer on a Delphi Programming course for undergraduate students in Mathematics.',
+            '\u2022 Teaching Assistant for a course on C++ and Windows API for undergraduate students in Computer Science.',
+            '\u2022 Teaching Assistant for a course on Delphi/Pascal for undergraduate students in Biology.',
+            '\u2022 Network administration (Windows Server and Active Directory, Debian Linux).'
+          ],
+          image: DefaultImages.UH
         }
       ]
     },
     {
       id: 'projects',
       title: 'Projects',
+      description: 'Some examples of project where I have participated as part of a team',
       backgroundImage: DefaultImages.SignalsDark,
       portfolioPageItems: [
         {
-          title: 'Project 1',
-          subTitle2: 'Cloud System',
-          contents:
-            'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-          image: DefaultImages.Signals
+          title: 'Aruba OS',
+          subTitle2: 'HPE Aruba, Montreal, Canada',
+          contents: [
+            'Aruba OS is a Network Management System for HPE Aruba Access Points and Controllers.',
+            'A web application is provided at each management level.',
+            'Additional web applications serve as troubleshooting tools.',
+            'Development for Aruba OS involves embedded linux, ANSI C, AngularJS, Docker.'
+          ],
+          image: DefaultImages.AOS
         },
         {
-          subTitle1: '2001-2004',
-          title: 'Project 2',
-          subTitle2: 'Video Streaming System',
+          title: 'Amyuni PDF Converter',
+          subTitle2: 'Amyuni Technologies, Montreal, Canada',
           contents:
-            'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-          image: DefaultImages.Music
+            'Amyuni PDF Converter is a software tool that allows users to create and edit digital documents such as PDF, PostScript or XPS. It is composed by a Microsoft Certified virtual printer driver that developers can use and control from a Windows application. It was implemented in a mix of C and C++',
+          image: DefaultImages.AmyuniPdfConverter
+        },
+        {
+          title: 'NewDj',
+          subTitle2: 'GMixon, France',
+          contents: [
+            'NewDJ is a Windows application integrated to DJ consoles for creating audio mix and applying effects.',
+            ' It was implemented using Delphi, C++ and DirectSound'
+          ],
+          image: DefaultImages.NewDJ
+        },
+        {
+          title: 'RadioSys',
+          subTitle2: 'Cuban Institute of Radio and Television, Havana, Cuba',
+          contents:
+            'RadioSys is a Windows application for radio stations with content management, multi-channel support and sound effects. The client component (player) was implemented using C++ and Delphi, DirectX. Server-side application was implemented using .Net Framework/C# and PostgreSQL.',
+          image: DefaultImages.RadioSys
         }
       ]
     }
